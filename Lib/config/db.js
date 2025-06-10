ef4ef1354462b7ce7ea330bafb756beced424221
petra-Nam <petranamatovu@gmail.com>
2025-06-10 18:58:12 +0200

    update

--- Lib/config/db.js
+++ Lib/config/db.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { combineTableNames } from "sequelize/types/utils";
 
 export const ConnectDB = async()=>{
     await mongoose.connect('mongodb+srv://sossumi:<wv53rW8fwO0Ysv5u>@cluster0.8yfsgki.mongodb.net/sosumi-blog')
